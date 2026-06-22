d69e9dd49bc38d3b6b562d007f38d42d19b4916e
christo <126384113+Christo790@users.noreply.github.com>
2026-06-22 20:03:43 +0530

    Create program4.cpp

---
+++ SEM3/DATA_STRUCTURE_LAB/program4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[20], i, n, pos, key, flag = 0;
+
+    cout << "Enter the range" << endl;
+    cin >> n;
+
+    cout << "Enter the elements" << endl;
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+
+    cout << "Enter the element to search" << endl;
+    cin >> key;
+
+    for (i = 0; i < n; i++)
+    {
+        if (key == a[i])
+        {
+            flag = 1;
+            pos = i;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        cout << "The element found in position " << pos << endl;
+    else
+        cout << "The element is not found in the list" << endl;
+
+    return 0;
+}
